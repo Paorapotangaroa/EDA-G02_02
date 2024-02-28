7fee6e70e3bdc65d2c86ae5f035c02f7c56d0e65
Riley_Marshall <rileyrmarshall1@gmail.com>
2024-02-28 10:18:06 -0700

    Added some independent variable analysis

--- EDA_Toa.R
+++ EDA_Toa.R
@@ -82,3 +82,52 @@ stacked_area_chart <- ggplot(binary_data, aes(x = seq_along(variable), fill = fa
   labs(title = "Stacked Area Chart of Binary Variable",
        x = "Index",
        y = "Count")
+
+
+# 10. Chi Square Test Tibble w/ Categorical Data
+
+cat_data <- data.frame(lapply(my_data, as.factor))
+cat_data <- cat_data[-1, ]
+
+    # Prepare a vector of variable names, excluding 'Label'
+variable_names <- names(cat_data)[!names(cat_data) %in% "Label"]
+
+    # Initialize an empty list to store the results
+results_list <- list()
+
+    # Loop through each variable to perform the Chi-squared test
+for(var_name in variable_names) {
+  # Perform the Chi-squared test
+  test_result <- chisq.test(table(cat_data[[var_name]], cat_data$Label))
+  
+  # Store the results with the variable name
+  results_list[[var_name]] <- broom::tidy(test_result) %>%
+    mutate(variable = var_name)
+}
+
+    # Combine the results into a single dataframe
+results_df <- bind_rows(results_list, .id = "variable")
+
+    # Correcting the issue by ensuring variable names are included
+results_df <- results_df %>%
+  select(variable, statistic = statistic, p.value = p.value, df = parameter)
+
+results_df$df <- NULL
+results_df
+
+# 11. Individual Variable Analysis (Confusin Matrix and Graphs)
+
+    # Enter variable where GET_TASKS is
+cat_data %>%
+  group_by(Label, GET_TASKS) %>%
+  summarise(Count = n(), .groups = "drop") %>%
+  # Calculate the total count for each Label category
+  group_by(Label) %>%
+  # Add a new column for percentage
+  mutate(Total = sum(Count),
+         Percentage = (Count / Total) * 100)
+
+ggplot(cat_data, aes(x = GET_TASKS, fill = Label)) + 
+  geom_bar(position = "fill")+
+  labs(title = "Relationship between Variable and Malware Label")
+
